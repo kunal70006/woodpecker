f453b13fb76f8b5ffe8e120262455949e460bf28
Kunaldeep Singh <kunaldeep@pushowl.com>
2025-05-18 22:44:12 +0530

    hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,32 @@
 import { Navbar } from "./Navbar";
+import { Button } from "./ui/Button";
 
 export const Hero = () => {
   return (
-    <div className="flex flex-col items-center h-screen shadow">
+    <div className="flex flex-col">
       <Navbar />
-      <h1 className="text-4xl font-bold">Welcome to our restaurant</h1>
-      <p className="text-lg">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
-      </p>
+      <div className="relative w-full mt-8 flex justify-end bg-dark-brown h-screen items-center">
+        <div className="absolute left-[32rem] top-80 flex flex-col gap-4 text-2xl text-right text-white font-lobster w-1/3 z-10">
+          <h2 className="font-bold">Welcome to The Woodpecker Cafe!</h2>
+          <p className="font-medium text-6xl">Crafting Perfect </p>
+          <p className="font-medium text-6xl">Moments,</p>
+          <p className="font-medium text-6xl">One Meal At A Time</p>
+          <p className="text-lg font-sans mt-4">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem
+            sunt delectus error atque magnam dolores eligendi sed, veniam est
+            dolorem commodi aut sint vel adipisci minus architecto at quis
+            soluta!
+          </p>
+          <div className="font-sans mt-8">
+            <Button className="rounded-none hover:bg-white hover:text-dark-brown">
+              Explore more
+            </Button>
+          </div>
+        </div>
+        <div className="w-1/2">
+          <img src="/pizza.png" alt="hero" className="w-full scale-x-[-1]" />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,14 +3,14 @@ import { Button } from "./ui/Button";
 
 export const Navbar = () => {
   return (
-    <nav className="flex items-center  justify-center gap-8 text-dark-brown font-medium text-xl p-6 bg-transparent">
+    <nav className="flex items-center  justify-center gap-8 text-dark-brown font-medium text-xl bg-transparent">
       <img src="/logo.svg" alt="logo" className="w-10 h-10" />
       <Link href="/">Home</Link>
       <Link href="/menu">Our menu</Link>
       <Link href="/about">About</Link>
       <Link href="#">Gallery</Link>
       <Link href="/contact">Contact</Link>
-      <Button>Book reservation</Button>
+      <Button className="rounded-none">Book reservation</Button>
     </nav>
   );
 };
